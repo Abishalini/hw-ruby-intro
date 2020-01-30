4fceb8486489355180bdb5530b28afa9a9c5e515
Abishalini <ec2-user@ip-172-31-33-23.us-east-2.compute.internal>
2020-01-30 23:07:19 +0000

    Completed Part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -26,10 +26,14 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  # A multiple of 4 in binary is any binary numeber that ends with 00
+  return true if s == "0"
+	/^[10]*00$/.match(s) != nil  
 end
 
 # Part 3
